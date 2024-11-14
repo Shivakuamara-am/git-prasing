74be7e93dd27a5b059f3ffad17310efa4f877012
shivakumara a m <shivukumaraam@gmail.com>
2024-11-14 16:34:12 +0530

    conatct vinod tappa

--- App.tsx
+++ App.tsx
@@ -1,152 +1,144 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Alert, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
+import Checkbox from 'expo-checkbox'
 
 const App = () => {
-  const[result,setResult]=useState("")
-  const[input,setInput]=useState("")
+  const[name,setName]=useState("")
+  const[email,setEamil]=useState("")
+  const[mobile,setMobile]=useState("")
+  const[messgae,setMessage]=useState("")
+  const[agri,setAgri]=useState(false)
 
-  const handlevlaues=(value:string)=>{
-    setInput(input+value)
-  }
-  const reset=()=>{
-    setInput("")
-    setResult("")
-  }
-
-  const claucalte=()=>{
-    try{
-      setResult(eval(input).toString())
-
-    }catch(error){
-      setResult("Error")
-
+  const submit=()=>{
+    if(!name && !email && !mobile && !messgae && !agri){
+      Alert.alert("enter all filses")
+    }
+    else{
+      Alert.alert(`Thank you${name}`)
     }
   }
 
 
 
-
   return (
-    <View style={styles.container}>
-      <TextInput
-      style={styles.input}
-      placeholder='Eneter data'
-      editable={false}
-      value={input}/>
-      <Text style={styles.result}>{result}</Text>
-      <View style={styles.buttonRow}>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("1")}>
-      <Text style={styles.buttonText}>1</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("2")}>
-      <Text style={styles.buttonText}>2</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("3")}>
-      <Text style={styles.buttonText}>3</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("+")}>
-      <Text style={styles.buttonText}>+</Text>
-    </TouchableOpacity>
+    <ScrollView>
+      <View style={styles.mainContainer}>
+      <Text style={styles.mainHeader}>Level up Your Knowledg</Text>
+      <Text style={styles.description}>You can reach us anytime via thapa @gmil.com</Text>
+       <View style={styles.inputContainer}>
+        <Text style={styles.labels}>Enter your Name</Text>
+          <TextInput
+          style={styles.inputStyle}
+          placeholder='Enter name'
+          value={name}
+          onChangeText={setName}
+           />
+       </View>
+       <View style={styles.inputContainer}>
+        <Text>Enter your email</Text>
+        <TextInput
+        style={styles.inputStyle}
+        placeholder='email'
+        value={email}
+        onChangeText={setEamil}/>
+       </View>
+
+       <View style={styles.inputContainer}>
+        <Text style={styles.labels}>Enter your mobile</Text>
+        <TextInput style={styles.inputStyle}
+        keyboardType="phone-pad"
+        placeholder='mobile num'/>
+      
+       </View>
+       <View style={styles.inputContainer}>
+        <Text style={styles.labels}>how can i help you</Text>
+        <TextInput style={styles.inputStyle}
+        placeholder='tell about your self'/>
+       </View>
+       <View style={styles.wrapper}>
+           <Checkbox value={agri}
+             
+           onValueChange={()=>setAgri(!agri)}
+           color={agri?"#c6624d":undefined}/>
+           <Text style={styles.wrapperText}>I have agri with your TC</Text>
+       </View>
+       <TouchableOpacity style={[styles.buttonStyle,
+        {backgroundColor:agri?"#c6624d":"#53c64d"}]} onPress={submit}>
+        <Text style={styles.buttonText}>Conact us</Text>
+       </TouchableOpacity>
+
     </View>
-    <View style={styles.buttonRow}>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("4")}>
-      <Text style={styles.buttonText}>4</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("5")}>
-      <Text style={styles.buttonText}>5</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("6")}>
-      <Text style={styles.buttonText}>6</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("-")}>
-      <Text style={styles.buttonText}>-</Text>
-    </TouchableOpacity>
-    </View>
-    <View style={styles.buttonRow}>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("7")}>
-      <Text style={styles.buttonText}>7</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("8")}>
-      <Text style={styles.buttonText}>8</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("9")}>
-      <Text style={styles.buttonText}>9</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("*")}>
-      <Text style={styles.buttonText}>*</Text>
-    </TouchableOpacity>
-    </View>
-    <View style={styles.buttonRow}>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("0")}>
-      <Text style={styles.buttonText}>0</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues(".")}>
-      <Text style={styles.buttonText}>.</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={claucalte}>
-      <Text style={styles.buttonText}>=</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("/")}>
-      <Text style={styles.buttonText}>/</Text>
-    </TouchableOpacity>
-    </View>
-    <TouchableOpacity style={[styles.button,styles.clearButton]} onPress={reset}>
-      <Text style={styles.buttonText}>c</Text>
-    </TouchableOpacity>
-    
-    </View>
+    </ScrollView>
   )
 }
 
 export default App
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-    backgroundColor: '#fff',
+  mainContainer: {
+    height: "100%",
+    paddingHorizontal: 30,
+    backgroundColor: "#fff",
   },
-  input: {
-    height: 60,
-    width: '100%',
-    fontSize: 24,
-    textAlign: 'right',
-    borderColor: '#ccc',
+  mainHeader: {
+    fontSize: 20,
+    color: "#344055",
+    fontWeight: "500",
+    paddingTop: 20,
+    paddingBottom: 15,
+    fontFamily: "JosefinSans_500Medium",
+    textTransform: "capitalize",
+  },
+  description: {
+    fontSize: 20,
+    color: "#7d7d7d",
+    paddingBottom: 20,
+    fontFamily: "JosefinSans_400Light",
+    lineHeight: 25,
+  },
+
+  inputContainer: {
+    marginTop: 20,
+  },
+  labels: {
+    fontWeight: "bold",
+    // fontSize: 15,
+    color: "#7d7d7d",
+    paddingBottom: 5,
+    fontFamily: "JosefinSans_300Light",
+    lineHeight: 25,
+  },
+  inputStyle: {
     borderWidth: 1,
-    marginBottom: 20,
-    padding: 10,
-    borderRadius: 8,
-    
+    borderColor: "rgba(0, 0, 0, 0.3)",
+    paddingHorizontal: 15,
+    paddingVertical: 6,
+    borderRadius: 2,
   },
-  result: {
-    fontSize: 32,
-    marginBottom: 20,
-    color: '#007BFF',
+  multiineStyle: {
+    paddingVertical: 4,
   },
-  buttonRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-    marginBottom: 10,
-  },
-  button: {
-    width: 70,
-    height: 70,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f1f1f1',
-    borderRadius: 35,
+  buttonStyle: {
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 18,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 30,
   },
   buttonText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
+    color: "#eee",
   },
-  clearButton: {
-    backgroundColor: '#FF6347',
-    width: '100%',
+  wrapper: {
+    display: "flex",
+    flexDirection: "row",
     marginTop: 20,
+    fontFamily: "JosefinSans_300Light",
+  },
+  wrapperText: {
+    marginLeft: 10,
+    color: "#7d7d7d",
+    fontFamily: "JosefinSans_300Light",
   },
 })
