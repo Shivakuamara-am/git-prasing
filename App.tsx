6ee486c9a1481b85832d878a6db9195446ad6f58
shivakumara a m <shivukumaraam@gmail.com>
2024-11-14 14:40:24 +0530

    thi is calucltor

--- App.tsx
+++ App.tsx
@@ -1,103 +1,99 @@
-import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 
 const App = () => {
- const[name,setName]=useState("")
- const[email,setEmail]=useState("")
- const[phone,setPhone]=useState("")
- const[gender,setGender]=useState("")
- const [error,setError]=useState({
-  name:"",
-  email:"",
-  phone:"",
-  gender:"",
+  const[result,setResult]=useState("")
+  const[input,setInput]=useState("")
 
- })
-
- const validateEmail=()=>{
- 
-  let regex= /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  return regex.test(email)
- }
-
- const validtionFrom=()=>{
-  let isvalid=true
-  let errorMeassge={name:"",email:"",phone:"",gender:"",}
-
- if(!name){
-  errorMeassge.name="Pls Enter name"
-  isvalid=false
- }
-
- if(!email){
-  errorMeassge.email="pls enter the email"
-  isvalid=false
-  
- }else if(!validateEmail(email)){
-  errorMeassge.email="Enter valid email addres"
-  isvalid=false
- }
-
- if(!phone){
-  errorMeassge.phone="pls enter the phonnumber"
-  isvalid=false
- }
-
- if(!gender){
-  errorMeassge.gender="pls enter the gender"
-  isvalid=false
- }
- setError(errorMeassge)
- return isvalid
- 
-  
- }
-
- const allValidation=()=>{
-  if(validtionFrom()){
-    Alert.alert(`form submit,Name${name},Email${email},Phone${phone},gender${gender}`)
- }
-  else{
-    Alert.alert("validation faild pls chacke the form and fill")
+  const handlevlaues=(value:string)=>{
+    setInput(input+value)
   }
-}
+  const reset=()=>{
+    setInput("")
+    setResult("")
+  }
+
+  const claucalte=()=>{
+    try{
+      setResult(eval(input).toString())
+
+    }catch(error){
+      setResult("Error")
+
+    }
+  }
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Admission Form</Text>
       <TextInput
       style={styles.input}
-      placeholder='Name'
-      value={name}
-      onChangeText={setName}/>
-      {error.name?<Text style={styles.error}>{error.name}</Text>:null}
-
-
-      <TextInput 
-      style={styles.input}
-      placeholder='email'
-      value={email}
-      onChangeText={setEmail}/>
-      {error.email?<Text style={styles.error}>{error.email}</Text>:null}
-
-      <TextInput
-      style={styles.input}
-      placeholder='phone'
-      keyboardType='phone-pad'
-      onChangeText={setPhone}
-      value={phone}
-      />
-      {error.phone?<Text style={styles.error}>{error.phone}</Text>:null}
-
-      <TextInput
-      style={styles.input}
-      placeholder='Male/Female'
-      onChangeText={setGender}
-      value={gender}/>
-      {error.gender?<Text style={styles.error}>{error.gender}</Text>:null}
-
-      <TouchableOpacity style={styles.button} onPress={allValidation}>
-        <Text style={styles.buttonText}>Submit</Text>
-      </TouchableOpacity>
+      placeholder='Eneter data'
+      editable={false}
+      value={input}/>
+      <Text style={styles.result}>{result}</Text>
+      <View style={styles.buttonRow}>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("1")}>
+      <Text style={styles.buttonText}>1</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("2")}>
+      <Text style={styles.buttonText}>2</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("3")}>
+      <Text style={styles.buttonText}>3</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("+")}>
+      <Text style={styles.buttonText}>+</Text>
+    </TouchableOpacity>
+    </View>
+    <View style={styles.buttonRow}>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("4")}>
+      <Text style={styles.buttonText}>4</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("5")}>
+      <Text style={styles.buttonText}>5</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("6")}>
+      <Text style={styles.buttonText}>6</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("-")}>
+      <Text style={styles.buttonText}>-</Text>
+    </TouchableOpacity>
+    </View>
+    <View style={styles.buttonRow}>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("7")}>
+      <Text style={styles.buttonText}>7</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("8")}>
+      <Text style={styles.buttonText}>8</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("9")}>
+      <Text style={styles.buttonText}>9</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("*")}>
+      <Text style={styles.buttonText}>*</Text>
+    </TouchableOpacity>
+    </View>
+    <View style={styles.buttonRow}>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("0")}>
+      <Text style={styles.buttonText}>0</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues(".")}>
+      <Text style={styles.buttonText}>.</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={claucalte}>
+      <Text style={styles.buttonText}>=</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.button} onPress={()=>handlevlaues("/")}>
+      <Text style={styles.buttonText}>/</Text>
+    </TouchableOpacity>
+    </View>
+    <TouchableOpacity style={[styles.button,styles.clearButton]} onPress={reset}>
+      <Text style={styles.buttonText}>c</Text>
+    </TouchableOpacity>
+    
     </View>
   )
 }
@@ -112,35 +108,45 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#fff',
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
   input: {
+    height: 60,
     width: '100%',
-    padding: 12,
-    marginBottom: 10,
+    fontSize: 24,
+    textAlign: 'right',
+    borderColor: '#ccc',
     borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 4,
+    marginBottom: 20,
+    padding: 10,
+    borderRadius: 8,
+    
+  },
+  result: {
+    fontSize: 32,
+    marginBottom: 20,
+    color: '#007BFF',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 10,
   },
   button: {
-    backgroundColor: '#4CAF50',
-    padding: 15,
-    borderRadius: 5,
-    marginTop: 10,
-    width: '100%',
+    width: 70,
+    height: 70,
+    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#f1f1f1',
+    borderRadius: 35,
   },
   buttonText: {
-    color: '#fff',
-    fontSize: 16,
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
   },
-  error: {
-    color: 'red',
-    fontSize: 12,
-    marginTop: -8,
-    marginBottom: 10,
+  clearButton: {
+    backgroundColor: '#FF6347',
+    width: '100%',
+    marginTop: 20,
   },
 })
